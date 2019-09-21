a8f0aa5611c0f363dd4cc6e4b9604127fb183230
fireairforce <1344492820@qq.com>
2019-09-21 21:48:18 +0800

    feat:add express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
+import express from "express";
+// import http from "http";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
 createConnection();
+
+const app = express();
+
+// const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+    res.json({ promise: "hello world" });
+});
+
+app.listen(3000);
