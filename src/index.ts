790b51f323e9768c3dd739c24b13bf9b25923021
fireairforce <1344492820@qq.com>
2019-09-22 11:32:40 +0800

    feat:完成express增删改查的构建

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,68 @@
-import express from "express";
+import bodyParser from "body-parser";
+import express, { NextFunction, Request, Response } from "express";
 import http from "http";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
+import { createConnection, getConnection, getManager } from "typeorm";
+import { WorkItem } from "./entity/WorkItem";
 
 createConnection();
 
 const app = express();
-
-const server = http.createServer(app);
+const router = express.Router();
 
 app.get("/", (req, res) => {
-    res.json({ promise: "hello world" });
+  res.json({ promote: "hello world" });
 });
+
+router.get("", async (req, res, next) => {
+  const workItemRepository = getConnection().manager.getRepository(WorkItem);
+  try {
+    const workItems = await workItemRepository.find({
+      order: { createAt: "DESC" },
+    });
+    res.json(workItems);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("", async (req, res, next) => {
+  const workItem = new WorkItem();
+  workItem.text = req.body.text;
+  const workItemRepository = getManager().getRepository(WorkItem);
+  try {
+    res.json(await workItemRepository.save(workItem));
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  const { body } = req;
+  const workItemRepository = getConnection().manager.getRepository(WorkItem);
+  try {
+    await workItemRepository.update(req.params.id, {
+      isChecked: body.isChecked,
+    });
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const workItemRepository = getConnection().manager.getRepository(WorkItem);
+  try {
+    await workItemRepository.delete(req.params.id);
+    res.sendStatus(204);
+  } catch (error) {
+      next(error);
+  }
+});
+
+app.use(bodyParser.json());
+app.use("/work-items", router);
+
+const server = http.createServer(app);
 
 app.listen(3001);
